8f998b6ac3e93af71371f0c90a663ca9995154a3
Helios <35069323+wlhcode@users.noreply.github.com>
2019-12-30 13:48:25 +0800

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WASP")]
+[assembly: AssemblyDescription("Welcome to WhatsApp Spammer Plus. The original version (simply referred to as WhatsApp Spammer) is created by Clement Hui in Python. WASP is an upgrade version to the original programme. \nWASP must be used with caution and is only intended for educational purposes. The programme must not be abused in any other way. The author of the programme is not responsible for any glitches or any damage done to your computer.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Clement Hui && Silas Wu")]
+[assembly: AssemblyProduct("WhatsApp Spammer Plus")]
+[assembly: AssemblyCopyright("© 2019, copyright reserved")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4df83fea-1639-4423-8d9e-b8d53c5623c6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
